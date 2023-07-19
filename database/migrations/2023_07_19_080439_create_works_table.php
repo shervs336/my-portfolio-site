79dfb3614e11d94c6b4504640b04d89629486cc2
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2023-07-19 16:48:13 +0800

    create work model related files

---
+++ database/migrations/2023_07_19_080439_create_works_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->id();
+            $table->string('background_image_path')->nullable();
+            $table->string('logo_image_path')->nullable();
+            $table->string('overlay-color')->default('blue-500');
+            $table->string('caption')->nullable();
+            $table->string('website_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('works');
+    }
+};
