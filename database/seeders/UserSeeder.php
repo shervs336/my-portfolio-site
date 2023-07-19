d12f9da6b3085ddbce388c78bac73aca76bc16b2
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2023-07-19 16:00:13 +0800

    add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::create([
+            'name' =>'Sherwin Rhey Condez',
+            'email' => 'sherwinrheycondez@gmail.com',
+            'password' => bcrypt('secret'),
+            'email_verified_at' => now()
+        ]);
+    }
+}
