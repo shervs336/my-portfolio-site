c5fd3514fba5f5d13a1e439f4315638964364c3e
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2023-09-05 17:31:51 +0800

    fix incorrect reference no livewire

--- resources/views/admin/works/index.blade.php
+++ resources/views/admin/works/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="flex flex-col min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
-        <h1 class="text-4xl font-bold mb-3">Blogs</h1>
-        @livewire('admin.blog')
+        <h1 class="text-4xl font-bold mb-3">Works</h1>
+        @livewire('admin.work')
     </div>
 @endsection
