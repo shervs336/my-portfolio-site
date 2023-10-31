b2511646296c08c070a8818da0b771edc74fc86e
Maybelle Sta.Maria <maybellestamaria18@gmail.com>
2023-10-31 17:33:37 +0800

    removed "s"

--- resources/views/admin/labs/index.blade.php
+++ resources/views/admin/labs/index.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <div class="flex flex-col min-h-screen py-12 bg-blue-50 sm:px-6 lg:px-8">
         <h1 class="text-4xl font-bold mb-3">Labs</h1>
-        @livewire('admin.labs')
+        @livewire('admin.lab')
     </div>
 @endsection
