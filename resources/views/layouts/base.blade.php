0c0ba7d52fed32f12f645c7350244092ccc5951a
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2023-11-05 02:07:19 +0800

    add coffee widget

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -27,4 +27,5 @@
     <body>
         @yield('body')
     </body>
+    <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="sherwinrheycondez" data-description="Support me on Buy me a coffee!" data-message="Thank you for visiting. You can also help me by buying me a coffee." data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
 </html>
