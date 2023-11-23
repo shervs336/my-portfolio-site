c7cf9aeb621d2a7d997c63fd363c65a214fc1f50
Maybelle Sta.Maria <maybellestamaria18@gmail.com>
2023-11-23 09:54:36 +0800

    removed from section in footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,40 +1,6 @@
 <br><br>
 
-<!--Register Form section-->
-<div class="flex justify-center" id="about" >
-    <div class="w-full py-12">
-        <div class="container margin:auto mx-auto flex flex-col space-y-4 md:space-y-0 md:space-x-4 md:flex-row justify-between">
-            <div class="w-full flex flex-col px-4">
-                <div class="flex-1">
-                        <h1 class="text-2xl font-bold text-blue-500">Get in touch 🤙</h1>
-                        <h2 class="text-5xl font-bold mt-2">Let's Collaborate!</h2>
-                        <p class="mt-4 text-sm text-gray-400">Have a Project in Mind or Just Want to Get in Touch?</p>
-                </div>
-            </div>
-            <div class="w-2/5 flex flex-col items-start ">
-                <form>
-                    <div class="mb-6">
-                        <label for="email" class="block py-2 text-sm font-bold text-gray-900 dark:text-white">Email</label>
-                        <input type="email" id="email" class="pr-4 peer block min-h-[auto] w-full rounded border border-gray-300 bg-slate-50 px-1 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-secondary-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" placeholder="youremail@example.com" required>
-                    </div>
-                    <div class="mb-6">
-                        <label for="password" class="block py-2 text-sm font-bold text-gray-900 dark:text-white">Subject</label>
-                        <input type="password" id="password" class="peer block min-h-[auto] w-full rounded border border-gray-300 bg-slate-50 px-1 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-secondary-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" required>
-                    </div>
-                    <div class="mb-6">
-                        <label for="message" class="block py-2 text-sm font-bold text-gray-900 dark:text-gray-900">Your message</label>
-                        <textarea id="message" rows="6" class="peer block min-h-[auto] w-full rounded border border-gray-300 bg-slate-50 px-1 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-secondary-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" placeholder="Type your message here..."></textarea>
-                    </div>
-                    
-                    <button type="submit" class="block rounded bg-blue-600 hover:bg-blue-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-                            data-te-ripple-init data-te-ripple-color="dark">Send</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
 
-<br><br>
         
 <!--Footer--> 
 <footer class="container margin:auto flex flex-col space-y-4 space-x-2 md:space-y-0 md:space-x-4  md:flex-row justify-between">
