380c08df453413c77529a1ef18d638820f81e007
Maybelle Sta.Maria <maybellestamaria18@gmail.com>
2023-11-29 16:31:15 +0800

    make route for blogs

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,13 +4,13 @@
             <a href="#" class="text-2xl font-bold">	&lt;/SRC&gt;</a>
 
             <div class="hidden md:flex md:items-center space-x-4">
-                <a href="{{ route('admin.index', ['test' => 'test']) }}" class=" hover:text-blue-500">Home</a>
+                <a href="{{ route('home') }}" class=" hover:text-blue-500">Home</a>
                 <a href="#about" class=" hover:text-blue-500">About</a>
                 <a href="#contact" class="hover:text-blue-500">Contact</a>
-                <a href="#latest-blog" class="hover:text-blue-500">Blog</a>
+                <a href="{{ route('blog.index') }}" class="hover:text-blue-500">Blog</a>
                 <a href="#labs"  class="hover:text-blue-600 py-2">Labs</a>
                 @if (Route::has('login'))
-                    
+    
                     @auth
                         <a href="{{ route('admin.index') }}" class="hover:text-gray-100 hover:bg-blue-900 bg-blue-700 text-white py-2 px-4 rounded-full">Dashboard</a>
                     @endauth
@@ -37,10 +37,10 @@
 
         <div x-show="open" class="md:hidden">
             <div class="px-2 pt-2 pb-4">
-                <a href="#home" class="block text-slate-300 font-semibold hover:text-blue-600 py-2">Home</a>
+                <a href="{{ route('home') }}" class="block text-slate-300 font-semibold hover:text-blue-600 py-2">Home</a>
                 <a href="#about" class="block text-slate-300 font-semibold hover:text-blue-600 py-2">About</a>
                 <a href="#contact" class="block text-slate-300 font-semibold hover:text-blue-600 py-2">Contact</a>
-                <a href="#latest-blog" class="block text-slate-300 font-semibold hover:text-blue-600 py-2">Blog</a>
+                <a href="{{ route('blog.index') }}" class="block text-slate-300 font-semibold hover:text-blue-600 py-2">Blog</a>
                 <a href="#labs" class="block text-slate-300 font-semibold hover:text-blue-600 py-2">Labs</a>
                 
             </div>
