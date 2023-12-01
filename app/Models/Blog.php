cdbc1867f67a38acbbbbf7df75c130e40d7f65f3
Maybelle Sta.Maria <maybellestamaria18@gmail.com>
2023-12-01 18:00:31 +0800

    storage for image for blogs

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Blog extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function image_path() 
+    {
+        return Storage::url($this->main_image);
+    }
 }
