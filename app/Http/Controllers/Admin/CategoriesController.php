688e2f3ef38e751303e7acc31a7bfa0c02914cec
Maybelle Sta.Maria <maybellestamaria18@gmail.com>
2024-01-17 14:00:37 +0800

    created categories controller

---
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Blog;
+
+class BlogsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function __invoke()
+    {
+        return view('admin.categories.index');
+    }
+}
