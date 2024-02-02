b811dc00eed90d0b3bfed0953f18af246001d3ce
Maybelle Sta.Maria <maybellestamaria18@gmail.com>
2024-02-02 20:37:40 +0800

    change text color and font weight to semibold

--- resources/views/components/showroom-content.blade.php
+++ resources/views/components/showroom-content.blade.php
@@ -1,12 +1,12 @@
 
-<div class="max-w-sm rounded overflow-hidden shadow-lg brightness-90">
+<div class="max-w-sm rounded overflow-hidden shadow-lg brightness-95">
     <img class="w-full rounded-lg h-64" src="{!!asset('storage/'.$work->background_image_path)!!}" alt="Sunset in the mountains">
         <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">{{$work->title}}</div>
-            <p class="text-gray-700 text-base">{{$work->caption}}</p>
+            <div class="font-semibold text-xl mb-2">{{$work->title}}</div>
+            <p class="text-gray-400 text-base">{{$work->caption}}</p>
         </div>
      <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{$work->website_link}}</span> 
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-500 mr-2 mb-2">{{$work->website_link}}</span> 
   </div>
 </div>
                                
