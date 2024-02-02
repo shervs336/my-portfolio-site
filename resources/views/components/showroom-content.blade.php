4786c9734ec5c5aceeced317963554c301b530fe
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2024-02-02 23:54:49 +0800

    display tags on works

--- resources/views/components/showroom-content.blade.php
+++ resources/views/components/showroom-content.blade.php
@@ -1,12 +1,16 @@
 
 <div class="max-w-sm rounded overflow-hidden shadow-lg brightness-95 ">
-    <img class="w-full md:w-full rounded-lg h-64" src="{!!asset('storage/'.$work->background_image_path)!!}" alt="work">
-        <div class="px-6 py-4">
-            <div class="font-semibold text-xl mb-2">{{$work->title}}</div>
-            <p class="text-gray-400 text-base">{{$work->caption}}</p>
-        </div>
-     <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-500 mr-2 mb-2">{{$work->website_link}}</span> 
+  <img class="w-full md:w-full rounded-lg h-64" src="{!!asset('storage/'.$work->background_image_path)!!}" alt="work">
+  <div class="px-6 py-4">
+      <div class="font-semibold text-xl mb-2">{{$work->title}}</div>
+      <p class="text-gray-400 text-base">{{$work->caption}}</p>
+  </div>
+  <div class="px-6 pt-4 pb-2"> 
+    @if($work->stack)
+      @foreach(explode(',', $work->stack) as $tech)
+        <span class="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-500 mr-2 mb-2 tag-{{$tech ?? 'default'}}">{{ $tech }}</span> 
+      @endforeach
+    @endif
   </div>
 </div>
                                
