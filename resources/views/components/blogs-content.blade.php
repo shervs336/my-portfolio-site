b444a5f845938f8977e56288e97f302c1cba6e71
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2024-02-04 23:41:24 +0800

    fix blog layout

--- resources/views/components/blogs-content.blade.php
+++ resources/views/components/blogs-content.blade.php
@@ -1,10 +1,13 @@
-<div class="max-w-[18rem] md:max-w-[20rem] lg:max-w-[24rem] rounded-lg bg-white dark:bg-neutral-700">
+<div class="max-w-[18rem] md:max-w-[20rem] lg:max-w-[24rem] rounded-lg bg-white shadow-lg">
     <div class="relative overflow-hidden bg-cover bg-no-repeat">
-        <img class="w-full rounded-lg" src="{!! !empty($blog->main_image)? asset('storage/'.$blog->main_image): asset('images/default.webp')!!}" alt="" />
+        <img class="w-full rounded-t-lg" src="{!! !empty($blog->main_image)? asset('storage/'.$blog->main_image): asset('images/default.webp')!!}" alt="" />
     </div>
-    <div class="pt-6 pb-3">
-        <h3 class="mb-2 text-xl md:text-xl xl:text-2xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+    <div class="pt-6 pb-4 px-6">
+        <h3 class="mb-2 text-xl font-medium tracking-wide text-neutral-800 dark:text-neutral-50">
             <a href="{{ route('blog.view', ['slug' => $blog->slug]) }}">{{$blog->title}}</a>
         </h3>
+        <small class="text-gray-500">
+            <i class="fa-regular fa-clock fa-fw"></i></i> {{ $blog->updated_at->diffForHumans() }}
+        </small>
     </div>
 </div>
