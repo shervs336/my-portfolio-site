c2878c23f42c04c7fece60d25bb1f0a258d7abb3
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2024-02-26 21:13:32 +0800

    change the icon and its color

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,55 +5,43 @@
         <h1 class="text-4xl font-bold mb-3">Dashboard</h1>
 
            
-        <div class="flex flex-wrap">
-            <!--Card 1-->
-            <div class="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
-                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110">
-                <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M96 96c0-35.3 28.7-64 64-64H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H80c-44.2 0-80-35.8-80-80V128c0-17.7 14.3-32 32-32s32 14.3 32 32V400c0 8.8 7.2 16 16 16s16-7.2 16-16V96zm64 24v80c0 13.3 10.7 24 24 24H296c13.3 0 24-10.7 24-24V120c0-13.3-10.7-24-24-24H184c-13.3 0-24 10.7-24 24zm208-8c0 8.8 7.2 16 16 16h48c8.8 0 16-7.2 16-16s-7.2-16-16-16H384c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16h48c8.8 0 16-7.2 16-16s-7.2-16-16-16H384c-8.8 0-16 7.2-16 16zM160 304c0 8.8 7.2 16 16 16H432c8.8 0 16-7.2 16-16s-7.2-16-16-16H176c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16H432c8.8 0 16-7.2 16-16s-7.2-16-16-16H176c-8.8 0-16 7.2-16 16z" fill="#4299e1"/></svg>
-                    <h2 class="title-font font-medium text-3xl text-gray-600">2.7K</h2>
-                    <p class="leading-relaxed">Blog Visits</p>
+        <div class="flex flex-wrap justify-center">
+            <!-- Card 1-->
+            <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4">
+                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 flex flex-col items-center">
+                    <i class="fa-regular fa-newspaper text-sky-500 text-7xl"></i>
+                    <h2 class="title-font font-medium text-3xl text-gray-400">2.7K</h2>
+                    <p class="leading-relaxed text-gray-700">Blog Visits</p>
                 </div>
             </div>
 
             <!-- Card 2-->
-            <div class="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
-                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110">
-                <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 512 512">
-                    <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                    <path d="M0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128zm64 32v64c0 17.7 14.3 32 32 32H416c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32H96c-17.7 0-32 14.3-32 32zM80 320c-13.3 0-24 10.7-24 24s10.7 24 24 24h56c13.3 0 24-10.7 24-24s-10.7-24-24-24H80zm136 0c-13.3 0-24 10.7-24 24s10.7 24 24 24h48c13.3 0 24-10.7 24-24s-10.7-24-24-24H216z" fill="#4299e1"/>
-                </svg>
-                    <h2 class="title-font font-medium text-3xl text-gray-600">99</h2>
-                    <p class="leading-relaxed">Total Page Visits</p>
+            <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4">
+                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 flex flex-col items-center">
+                    <i class="fa-solid fa-pager text-sky-500 text-7xl"></i>
+                    <h2 class="title-font font-medium text-3xl text-gray-400">99</h2>
+                    <p class="leading-relaxed text-gray-700">Total Page Visits</p>    
                 </div>
             </div>
 
             <!-- Card 3-->
-            <div class="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
-                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110">
-                <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" fill="#4299e1"/></svg>
-                    <h2 class="title-font font-medium text-3xl text-gray-600">74</h2>
-                    <p class="leading-relaxed">Newsletter</p>
+            <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4">
+                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 flex flex-col items-center">
+                    <i class="fa-regular fa-envelope text-sky-500 text-7xl"></i>
+                    <h2 class="title-font font-medium text-3xl text-gray-400">74</h2>
+                    <p class="leading-relaxed text-gray-700">Newsletter Subscription</p>
                 </div>
             </div>
 
             <!-- Card 4-->
-            <div class="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
-                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110">
-                <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 96l576 0c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96zm0 32V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V128H0zM64 405.3c0-29.5 23.9-53.3 53.3-53.3H234.7c29.5 0 53.3 23.9 53.3 53.3c0 5.9-4.8 10.7-10.7 10.7H74.7c-5.9 0-10.7-4.8-10.7-10.7zM176 192a64 64 0 1 1 0 128 64 64 0 1 1 0-128zm176 16c0-8.8 7.2-16 16-16H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16zm0 64c0-8.8 7.2-16 16-16H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16zm0 64c0-8.8 7.2-16 16-16H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16z" fill="#4299e1"/></svg>
-                    <h2 class="title-font font-medium text-3xl text-gray-600">46</h2>
-                    <p class="leading-relaxed">Subscription</p>
+            <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4">
+                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 flex flex-col items-center">
+                    <i class="fa-solid fa-phone text-sky-500 text-7xl"></i>
+                    <h2 class="title-font font-medium text-3xl text-gray-400">46</h2>
+                    <p class="leading-relaxed text-gray-700">Contacts Received</p>
                 </div>
             </div>
-
-            <!-- Card 5-->
-            <div class="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
-                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110">
-                <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" fill="#4299e1"/></svg>
-                    <h2 class="title-font font-medium text-3xl text-gray-600">46</h2>
-                    <p class="leading-relaxed">Contacts Received</p>
-                </div>
-            </div>
-
         </div>
+
     </div>
 @endsection
