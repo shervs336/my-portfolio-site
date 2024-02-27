16a181cd7cb2fcd7a0eb5dfe29ac3b33af3cdaa7
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2024-02-27 10:48:08 +0800

    add dashboard temp placements

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,40 +5,56 @@
         <h1 class="text-4xl font-bold mb-3">Dashboard</h1>
 
            
-        <div class="flex flex-wrap justify-center">
+        <div class="grid sm:grid-cols-2 md:grid-cols-4 gap-x-4 gap-y-2">
             <!-- Card 1-->
-            <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4">
-                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 flex flex-col items-center">
-                    <i class="fa-regular fa-newspaper text-sky-500 text-7xl"></i>
-                    <h2 class="title-font font-medium text-3xl text-gray-400">2.7K</h2>
-                    <p class="leading-relaxed text-gray-700">Blog Visits</p>
+            <div class="w-full">
+                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 flex items-center">
+                    <div class="grow">
+                        <small class="leading-relaxed text-gray-400">Blog Visits</small>
+                        <div class="font-medium text-2xl text-gray-700">2.7K</div>
+                    </div>
+                    <div class="bg-sky-500 py-3 px-4 rounded-md shadow-md">
+                        <i class="fa-solid fa-blog text-white"></i>
+                    </div>
                 </div>
             </div>
 
             <!-- Card 2-->
-            <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4">
-                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 flex flex-col items-center">
-                    <i class="fa-solid fa-pager text-sky-500 text-7xl"></i>
-                    <h2 class="title-font font-medium text-3xl text-gray-400">99</h2>
-                    <p class="leading-relaxed text-gray-700">Total Page Visits</p>    
+            <div class="w-full">
+                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 flex items-center">
+                    <div class="grow">
+                        <small class="leading-relaxed text-gray-400">Total Page Visits</small>
+                        <div class="font-medium text-2xl text-gray-700">99</div>
+                    </div>
+                    <div class="bg-sky-500 py-3 px-4 rounded-md shadow-md">
+                        <i class="fa-regular fa-newspaper text-white"></i>
+                    </div>
                 </div>
             </div>
 
             <!-- Card 3-->
-            <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4">
-                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 flex flex-col items-center">
-                    <i class="fa-regular fa-envelope text-sky-500 text-7xl"></i>
-                    <h2 class="title-font font-medium text-3xl text-gray-400">74</h2>
-                    <p class="leading-relaxed text-gray-700">Newsletter Subscription</p>
+            <div class="w-full">
+                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 flex items-center">
+                    <div class="grow">
+                        <small class="leading-relaxed text-gray-400">Newsletter Subscription</small>
+                        <div class="font-medium text-2xl text-gray-700">99</div>
+                    </div>
+                    <div class="bg-sky-500 py-3 px-4 rounded-md shadow-md">
+                        <i class="fa-regular fa-envelope text-white"></i>
+                    </div>
                 </div>
             </div>
 
             <!-- Card 4-->
-            <div class="p-4 w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4">
-                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 flex flex-col items-center">
-                    <i class="fa-solid fa-phone text-sky-500 text-7xl"></i>
-                    <h2 class="title-font font-medium text-3xl text-gray-400">46</h2>
-                    <p class="leading-relaxed text-gray-700">Contacts Received</p>
+            <div class="w-full">
+                <div class="border-2 border-blue-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 flex items-center">
+                    <div class="grow">
+                        <small class="leading-relaxed text-gray-400">Contacts Received</small>
+                        <div class="font-medium text-2xl text-gray-700">99</div>
+                    </div>
+                    <div class="bg-sky-500 py-3 px-4 rounded-md shadow-md">
+                        <i class="fa-solid fa-phone text-white"></i>
+                    </div>
                 </div>
             </div>
         </div>
